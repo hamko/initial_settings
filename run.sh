55600aa9518dfeec50ac42e990f9e6d803492550
hamko <higepon@xxxx.com>
2013-08-24 11:42:03 +0900

    added dropbox installation

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
-sudo apt-get install aptitude 
-sudo aptitude install git
+sudo apt-get -y install aptitude 
+sudo aptitude -y install git sysv-rc-conf
+
 git config --global github.name hamko
 git config --global github.email !!!your email!!!
 git config --global credential.helper cache
@@ -21,3 +22,8 @@ git clone https://github.com/hamko/initial_settings.git
 git clone https://github.com/hamko/cppode.git
 git clone https://github.com/hamko/docs.git
 git clone https://github.com/hamko/sample.git
+
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+sudo cp dropbox /etc/init.d/dropbox
+sudo sysv-rc-conf dropbox on
+~/.dropbox-dist/dropboxd
