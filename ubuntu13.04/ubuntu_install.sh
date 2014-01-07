56385f4847d6e7f0d0c4661c80620ad494043e0a
hamko <wakataberyo@gmail.com>
2014-01-07 19:36:56 +0900

    a

--- ubuntu13.04/ubuntu_install.sh
+++ ubuntu13.04/ubuntu_install.sh
@@ -61,6 +61,7 @@ sudo aptitude -y install build-essential linux-headers-$(uname -r) open-vm-dkms
 sudo ln -s /usr/src/linux-headers-$(uname -r)/include/generated/uapi/linux/version.h /usr/src/linux-headers-$(uname -r)/include/linux/version.h
 sudo insmod /lib/modules/3.8.0-19-generic/misc/vmmon.ko
 sudo vmware-modconfig --console --install-all
+sudo service vmware start
 
 #recordmydesktop
 sudo aptitude -y install mencoder
