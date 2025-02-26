7e9dc1df449e6b854f522ca9d82cd597a7022a36
Ryo Wakatabe <wakataberyo@gmail.com>
2025-02-26 22:14:16 +0900

    fixed install.sh for the latest settings

--- ubuntu24.04/install.sh
+++ ubuntu24.04/install.sh
@@ -9,10 +9,6 @@ git config --global user.email "wakataberyo@gmail.com"
 git config --global user.name "Ryo Wakatabe"
 git config --global push.default simple
 
-mkdir git
-cd git
-git clone https://github.com/hamko/setting
-
 # fundamentals
 sudo apt -y install build-essential 
 sudo apt -y install valgrind gnuplot minicom cmake 
@@ -35,21 +31,21 @@ sudo apt -y install inkscape gimp vlc
 sudo apt -y install task-japanese-gnome-desktop language-pack-gnome-ja-base language-pack-gnome-ja gnome-user-docs-ja libreoffice-help-ja libreoffice-l10n-ja thunderbird-locale-ja fonts-noto-cjk-extra
 # Settings > Keyboard > Keyboard Shortcuts > Typing > Set Ctrl-Space
 
-# node.js
-sudo dpkg --remove --force-remove-reinstreq libnode-dev
-sudo dpkg --remove --force-remove-reinstreq libnode72:amd64
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt install -y nodejs
-
 # parallel
 sudo apt -y install parallel
 
 # GPU
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt --purge remove nvidia-*
-sudo apt --purge remove cuda-*
-sudo ubuntu-drivers autoinstall
-sudo apt install -y nvidia-cuda-toolkit
-sudo apt install -y nvtop
-nvidia-smi # To check if GPU is used
-nvtop # To check how GPU is used
+# sudo add-apt-repository ppa:graphics-drivers/ppa
+# sudo apt --purge remove nvidia-*
+# sudo apt --purge remove cuda-*
+# sudo ubuntu-drivers autoinstall
+# sudo apt install -y nvidia-cuda-toolkit
+# sudo apt install -y nvtop
+# nvidia-smi # To check if GPU is used
+# nvtop # To check how GPU is used
+
+mkdir git
+cd git
+git clone https://github.com/hamko/setting
+
+
